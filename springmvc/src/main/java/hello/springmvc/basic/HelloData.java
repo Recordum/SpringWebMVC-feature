3189c385832cae2c23fc384272553cd9c8d47dc3
Mingyu <92289805+Recordum@users.noreply.github.com>
2023-03-04 00:14:17 +0900

    springWebMVC - HTTP요청파라미터 @ModelAttribute

---
+++ springmvc/src/main/java/hello/springmvc/basic/HelloData.java
@@ -0,0 +1,12 @@
+package hello.springmvc.basic;
+
+import lombok.Data;
+
+/**
+ * @Data : getter/setter/toString 등
+ */
+@Data
+public class HelloData {
+    private String username;
+    private int age;
+}
